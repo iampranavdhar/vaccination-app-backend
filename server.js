d15760f4555fe730414b4fa2f718930266448f7d
iampranavdhar <nspr2002@gmail.com>
2021-08-30 16:15:32 +0530

    Cors Options Added

--- server.js
+++ server.js
@@ -13,9 +13,15 @@ dotenv.config()
 const app = express()
 const port = process.env.PORT || 5000
 
+const corsOptions = {
+    origin: '*',
+    credentials: true,            //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+}
+
 /* Middlewares */
 app.use(express.json())
-app.use(cors())
+app.use(cors(corsOptions))
 app.use(cookieParser());
 
 /* API Routes */
